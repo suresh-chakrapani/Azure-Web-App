785dac055ebb57596c534b0cf6b57848b29156c3
Suresh Chakrapani <61193970+suresh-chakrapani@users.noreply.github.com>
2025-01-17 15:48:58 +0530

    Added Azure Function call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   value = 'Suresh';
+  http: HttpClient;
+  constructor(private httpClient: HttpClient){
+    this.http = this.httpClient;
+  }
 
   onSubmit(textVal: string){
     this.value=textVal;
+
+    this.http.get('https://sureshpocanalyzeimage.azurewebsites.net/api/AnalyzeImage?name=SC')
+      .subscribe((resp: any) => this.value = resp.text);
   }
 }
