a596226cfbdf14b6bb6355b4dc8fbe92a969012a
Suresh Chakrapani <61193970+suresh-chakrapani@users.noreply.github.com>
2025-01-17 15:20:35 +0530

    Added html with a textbox and a button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   value = 'Suresh';
+
+  onSubmit(textVal: string){
+    this.value=textVal;
+  }
 }
